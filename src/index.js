9d0eed7cd7f3118b726053b9093d43e0ce2686aa
dad <60132399+dad2vad@users.noreply.github.com>
2020-02-07 21:50:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,114 +1,191 @@
-import React from "react";
-import ReactDOM from "react-dom";
 
-import "./styles.css";
-
-import { useForm, useField, splitFormProps } from "react-form";
-
-async function sendToFakeServer(values) {
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  return values;
-}
-
-function validateAddressStreet(value) {
-  if (!value) {
-    return "A street is required";
-  }
-  return false;
-}
-
-async function fakeCheckValidName(name, instance) {
-  if (!name) {
-    return "A name is required";
-  }
-  return instance.debounce(async () => {
-    console.log("checking name");
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    // All names are valid, so return a false error
-    return false;
-  }, 500);
-}
-
-const InputField = React.forwardRef((props, ref) => {
-  // Let's use splitFormProps to get form-specific props
-  const [field, fieldOptions, rest] = splitFormProps(props);
-
-  // Use the useField hook with a field and field options
-  // to access field state
-  const {
-    meta: { error, isTouched, isValidating },
-    getInputProps
-  } = useField(field, fieldOptions);
-
-  // Build the field
-  return (
-    <>
-      <input {...getInputProps({ ref, ...rest })} />{" "}
-      {isValidating ? (
-        <em>Validating...</em>
-      ) : isTouched && error ? (
-        <em>{error}</em>
-      ) : null}
-    </>
-  );
-});
-
-function MyForm() {
-  // Use the useForm hook to create a form instance
-  const {
-    Form,
-    meta: { isSubmitting, canSubmit }
-  } = useForm({
-    onSubmit: async (values, instance) => {
-      // onSubmit (and everything else in React Form)
-      // has async support out-of-the-box
-      await sendToFakeServer(values);
-      console.log("Huzzah!");
-    },
-    debugForm: true
+import { useState } from 'react';
+import 'bulma/css/bulma.min.css';
+import Layout from '../components/MyLayout'
+const Home = () => {
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    url: '',
+    honeypot: '', // if any value received in this field, form submission will be ignored.
+    message: '',
+    replyTo: '@', // this will set replyTo of email to email address entered in the form
+    tce: '' 
   });
 
+  const [response, setResponse] = useState({
+    type: '',
+    message: ''
+  });
+
+  const handleChange = e => {
+    setContact({ ...contact, [e.target.name]: e.target.value })
+  }
+
+  const TO = () => {
+    
+    const res = fetch('https://api.telegram.org/bot695543276:AAHqIsFuK-hzCor9q3nO2WgVlV6UfRFRE7c/sendMessage?chat_id=986940575&text=' + JSON.stringify(contact, null,4), {
+      method: 'POST'
+    })
+    const json = res.json()
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+TO(
+      if (json.success) {
+        setResponse({
+          type: 'success',
+          message: 'ПАСИБА ТАДА'
+        });
+      } else {
+        setResponse({
+          type: 'error',
+          message: json.message
+        });
+      }
+    } catch (e) {
+      console.log('An error occurred', e);
+      setResponse({
+        type: 'error',
+        message: JSON.stringify(e)
+      });
+    }
+  };
   return (
-    <Form>
-      <div>
-        <label>
-          Name: <InputField field="name" validate={fakeCheckValidName} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Address Street:{" "}
-          <InputField field="address.street" validate={validateAddressStreet} />
-        </label>
-      </div>
-          <div>
-        <label>
-          Url: <InputField field="url" validate={fakeCheckValidName} />
-        </label>
-      </div>
-      <div>
-        <label>
-          Pic:{" "}
-          <InputField field="pic" validate={validateAddressStreet} />
-        </label>
-      </div>
 
-      <div>
-        <button type="submit" disabled={!canSubmit}>
-          Submit
-        </button>
+    <div>
+      <div className='section'>
+        <div className='container'>
+          <div className='columns'>
+            <div className='column' />
+            <div className='column  is-two-thirds'>
+              <div
+                className={
+                  response.type === 'success'
+                    ? 'tile box notification is-primary'
+                    : 'is-hidden'
+                }
+              >
+                <p>{response.message}</p>
+              </div>
+              <div
+                className={
+                  response.type === 'error'
+                    ? 'tile box notification is-danger'
+                    : 'is-hidden'
+                }
+              >
+                <p>{response.message}</p>
+              </div>
+              <div
+                className={response.message !== '' ? 'is-hidden' : 'columns'}
+              >
+                <div className='column content'>
+                  <h2>ЗАЯВКА</h2>
+                  <form
+                    action='https://api.staticforms.xyz/submit'
+                    method='post'
+                    onSubmit={handleSubmit}
+                  >
+                    <div className='field'>
+                      <label className='label'>ШО</label>
+                      <div className='control'>
+                        <input
+                          className='input'
+                          type='text'
+                          placeholder='шо'
+                          name='name'
+                          onChange={handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>ХТО</label>
+                      <div className='control'>
+                        <input
+                          className='input'
+                          type='image'
+                          placeholder='pic'
+                          name='image'
+                          onChange={handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>URL</label>
+                      <div className='control'>
+                        <input
+                          className='input'
+                          type='url'
+                          placeholder='url'
+                          name='url'
+                          onChange={handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className='field' style={{ display: 'none' }}>
+                      <label className='label'>Title</label>
+                      <div className='control'>
+                        <input
+                          type='text'
+                          name='honeypot'
+                          style={{ display: 'none' }}
+                          onChange={handleChange}
+                        />
+                        <input
+                          type='hidden'
+                          name='subject'
+                          onChange={handleChange}
+                        />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>ПРО</label>
+                      <div className='control'>
+                        <textarea
+                          className='textarea'
+                          placeholder='про'
+                          name='message'
+                          onChange={handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className='field'>
+                      <label className='label'>ЦЕ</label>
+                      <div className='control'>
+                        <textarea
+                          className='textarea'
+                          placeholder='це'
+                          name='tce'
+                          onChange={handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+                    <div className='field is-grouped'>
+                      <div className='control'>
+                        <button className='button is-primary' type='submit'>
+                          ok
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className='column' />
+          </div>
+        </div>
       </div>
-
-      <div>
-        <em>{isSubmitting ? "Submitting..." : null}</em>
-      </div>
-    </Form>
-  );
+    </div>
+  )
 }
 
-function App() {
-  return <MyForm />;
-}
+export default Home;
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
